d94f073f7a53c3806db3ff2b516981000e3968ce
consolomon <72203471+consolomon@users.noreply.github.com>
2023-04-18 23:16:48 +0200

    Correcting commitment

--- src/sql/staging_ddl.sql
+++ src/sql/staging_ddl.sql
@@ -5,10 +5,10 @@ CREATE TABLE KOSYAK1998YANDEXRU__STAGING.group_log (
     user_id integer NOT NULL,
     user_id_from integer,
     event varchar(20),
-    "datetime" timestamp(0),
-    CONSTRAINT group_log_pkey PRIMARY KEY (user_id, "datetime") 
+    event_ts timestamp(0),
+    CONSTRAINT group_log_pkey PRIMARY KEY (user_id, event_ts) 
 )
 ORDER BY group_id, user_id
 SEGMENTED BY hash(group_id) ALL NODES
-PARTITION BY "datetime"::Date
-GROUP BY CALENDAR_HIERARCHY_DAY(group_log."datetime"::Date, 3, 2);
+PARTITION BY event_ts::Date
+GROUP BY CALENDAR_HIERARCHY_DAY(group_log.event_ts::Date, 3, 2);
